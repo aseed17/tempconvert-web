8a4e369f42bb3be1dc8e133eaeaaf1eacdf7541d
aseed17 <128793741+aseed17@users.noreply.github.com>
2024-08-06 10:12:03 +0630

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+//console.log(`Hello Thae Ei Ko Ko`);
+//console.log(`I like Thae Ei Ko Ko`);
+
+//window.alert(`Warning This is an alert!`);
+//window.alert(`Chit tl nor ThaeThae`);
+
+let fullname = "Aung Pyae Sone";
+let age = 20;
+let isStudent = true;
+
+/*document.getElementById("p1").textContent = `Your full name is ${fullname}.`
+document.getElementById("p2").textContent = `Your age is ${age}.` 
+document.getElementById("p3").textContent = `Student: ${isStudent}`*/
